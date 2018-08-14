62805cb4a307c1bc87bd9ebc35d6fe118a95fb79
Aleksi Alajoki <fensnake@yahoo.com>
2018-08-14 21:52:05 +0300

    Renaming files with specific extensions

--- Rename.py
+++ Rename.py
@@ -10,35 +10,27 @@ def main():
         print("Welcome. Press CTRL + C to cancel anytime.")
         dir = dir_input()
 
+        #print("""
+        #(1) Rename everything
+        #(2) Rename all files
+        #(3) Rename all folders
+        #(4) Rename files with specific extensions
+        #""")
+
         print("""
-        (1) Rename everything
-        (2) Rename all files
-        (3) Rename all folders
-        (4) Rename specific filetypes
-        """)
+    (1) Rename all files
+    (2) Rename all folders
+    (3) Rename files with specific extensions
+    (4) Rename everything
+                """)
 
         option = options()
 
         basename = input("Enter the new name: ")
 
-        # Input: file extensions?
-        # Everything except folders
-        # Just folders
-        # Specific filetypes
-            # Input specific filetypes separated by a space
 
-
-        # Display the chosen action, ask for confirmation
         if option == 1:
-            for filename in os.listdir(dir):
-                if filename == os.path.basename(__file__):
-                    continue
-
-                ext = os.path.splitext(dir + "/" + filename)[1]
-                os.rename(dir + "/" + filename, dir + "/" + basename + str(i) + ext)
-                i += 1
-
-        elif option == 2:
+            # Display the chosen action, ask for confirmation
             for filename in os.listdir(dir):
                 if filename == os.path.basename(__file__) or os.path.isdir(dir + "/" + filename) == True:
                     continue
@@ -47,7 +39,8 @@ def main():
                 os.rename(dir + "/" + filename, dir + "/" + basename + str(i) + ext)
                 i += 1
 
-        elif option == 3:
+        elif option == 2:
+            # Display the chosen action, ask for confirmation
             for filename in os.listdir(dir):
                 if filename == os.path.basename(__file__) or os.path.isdir(dir + "/" + filename) == False:
                     continue
@@ -56,13 +49,35 @@ def main():
                 os.rename(dir + "/" + filename, dir + "/" + basename + str(i) + ext)
                 i += 1
 
-        #elif option == 4:
-            # Enter extensions separated by a space
+        elif option == 3:
+            exts = input("Enter all extensions of the files you want to rename. Separate them with spaces: ")
             # Do you want each extension to have its own numbering? For example, 1.jpg, 2.jpg, 1.png, 2.png
+            # Display the chosen action, ask for confirmation
+            exts_list = exts.split()
 
-        else:
-            print("asdasd")
-            break
+            exts_list = ["." + e.lower() for e in exts_list]
+
+            print(exts_list)
+
+            for filename in os.listdir(dir):
+                ext = os.path.splitext(dir + "/" + filename)[1]
+
+                if filename == os.path.basename(__file__) or os.path.isdir(dir + "/" + filename) == True or ext.lower() not in exts_list:
+                    continue
+
+                os.rename(dir + "/" + filename, dir + "/" + basename + str(i) + ext)
+                i += 1
+
+        elif option == 4:
+            for filename in os.listdir(dir):
+                if filename == os.path.basename(__file__):
+                    continue
+
+                ext = os.path.splitext(dir + "/" + filename)[1]
+                os.rename(dir + "/" + filename, dir + "/" + basename + str(i) + ext)
+                i += 1
+
+
         i = 1
         # Catch errors?
         print("Files renamed. Thank you, come again.")
